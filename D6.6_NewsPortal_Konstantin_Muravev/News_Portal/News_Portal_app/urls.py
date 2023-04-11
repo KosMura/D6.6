b24da174e0468d33debd8b1bcd548fa14f62522b
KosMura <120734483+KosMura@users.noreply.github.com>
2023-04-11 19:05:16 +0300

    Add files via upload

---
+++ D6.6_NewsPortal_Konstantin_Muravev/News_Portal/News_Portal_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import PostView, ViewPost
+
+urlpatterns = [
+    path('', PostView.as_view(), name='post_view'),
+    path('<int:pk>/', ViewPost.as_view(), name='view_post'),   # было 'news/<int:pk>/'
